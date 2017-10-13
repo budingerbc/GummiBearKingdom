4811216340a63452502dbd43539e9c663e48b16b
Ben Budinger <budingerbc@gmail.com>
2017-10-13 09:16:09 -0700

    Setup .NET MVC framework for Gummi Bear Kingdom web page.

---
+++ TravelBlog/Models/GummiBearKingdomContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiBearKingdomContext : DbContext
+    {
+        public GummiBearKingdomContext()
+        {
+        }
+
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+				.UseMySql(@"Server=localhost;Port=8889;database=GummiBearKingdom;uid=root;pwd=root;");
+        }
+
+		public GummiBearKingdomContext(DbContextOptions<GummiBearKingdomContext> options)
+            : base(options)
+        {
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+	}
+}
