5a3c5bb15aaf6c46232f4e4ff0ed86827fea9e1b
Ben Budinger <budingerbc@gmail.com>
2017-10-13 09:24:11 -0700

    Add migration/creation of database with Products table.

---
+++ TravelBlog/Migrations/20171013162004_Initial.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Migrations
+{
+    [DbContext(typeof(GummiBearKingdomContext))]
+    [Migration("20171013162004_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
---
+++ TravelBlog/Migrations/GummiBearKingdomContextModelSnapshot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Migrations
+{
+    [DbContext(typeof(GummiBearKingdomContext))]
+    partial class GummiBearKingdomContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
